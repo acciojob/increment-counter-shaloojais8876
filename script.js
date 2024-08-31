140099c6fa08f2f82a71cd804bbcd0e5437cdd08
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-08-31 14:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const para = document.getElementById("counter");
+const btn = document.getElementById("incrementBtn");
+let count = 0;
+incrementBtn.addEventListener("click", ()=>{
+
+	para.innerText = count++;
+	alert("un-incremented value");
+})
