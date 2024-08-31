7c39da06dc3a6ab377898ede85f4b4ce8b265dc6
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-08-31 14:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btn = document.getElementById("incrementBtn");
 
 incrementBtn.addEventListener("click", ()=>{
 let count = Number(para.innerText);
-	para.innerText = ++count;
 	alert(count);
+	para.innerText = ++count;
+	
 })
